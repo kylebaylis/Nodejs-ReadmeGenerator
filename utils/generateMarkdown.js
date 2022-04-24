00c12e9db3cfc84b0bbf680728f55a5eefa9e8d5
Kyle <kylejmbaylis@gmail.com>
2022-04-24 15:52:20 -0400

    reverted changes to generateMarkdown - now creates txt file

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -12,7 +12,7 @@ function renderLicenseSection(license) {}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
-  return `# ${answers.title}
+  return `# ${data.title}
 
 `;
 }
